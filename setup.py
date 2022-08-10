9d4322cc740a216a71f6036260f2ded722fc39dd
Alexey Kuznetsov <alexey.k.kuznetsov@gsk.com>
2022-08-10 10:50:12 +0000

    upversion

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ def read_text(file_name: str):
 
 setuptools.setup(
     name="neointerface",                     # This is the name of the package
-    version="3.1.4",                         # The initial release version
-    author="Alexey Kuznetsov, Julian West",  # Full name of the authors
+    version="3.1.5",                         # The initial release version
+    author="Alexey Kuznetsov, Julian West, Ben Grinsted, William McDermott",  # Full name of the authors
     description="A Python interface to use the Neo4j graph database",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
