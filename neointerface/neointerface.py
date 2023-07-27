dbb5f491f579a2729652764417ae1b84258e8bad
Vidhi Srivastava <vidhi.x.srivastava@gsk.com>
2023-07-27 12:53:10 +0000

    Changed the assert statements

--- neointerface/neointerface.py
+++ neointerface/neointerface.py
@@ -90,10 +90,10 @@ class NeoInterface:
                 self.rdf_setup_connection()
 
         
-        assert (os.environ.get("NEO4J_HOST"))!="" or (os.environ.get('NEO4J_HOST')) is not None, 'Enviorment var NEO4J_HOST is not defined'
-        assert (os.environ.get("NEO4J_USER"))!="" or (os.environ.get('NEO4J_USER')) is not None, 'Enviorment var NEO4J_USER is not defined'
-        assert (os.environ.get("NEO4J_PASSWORD"))!="" or (os.environ.get('NEO4J_PASSWORD')) is not None, 'Enviorment var NEO4J_PASSWORD is not defined'
-        assert (os.environ.get("NEO4J_RDF_HOST"))!="" or (os.environ.get('NEO4J_RDF_HOST')) is not None, 'Enviorment var NEO4J_RDF_HOST is not defined'
+        assert os.environ.get("NEO4J_HOST", "").strip(), 'Enviorment var NEO4J_HOST is not defined'
+        assert os.environ.get("NEO4J_USER", "").strip(), 'Enviorment var NEO4J_USER is not defined'
+        assert os.environ.get("NEO4J_PASSWORD", "").strip(), 'Enviorment var NEO4J_PASSWORD is not defined'
+        assert os.environ.get("NEO4J_RDF_HOST", "").strip(), 'Enviorment var NEO4J_RDF_HOST is not defined'
 
     def connect(self) -> None:
         try:
